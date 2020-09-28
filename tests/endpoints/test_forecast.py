c99b1089d068cb619816c54bf3b5f05e47befb61
ian douglas <ian.douglas@iandouglas.com>
2020-09-27 23:26:56 -0600

    added happy path test for forecast

---
+++ tests/endpoints/test_forecast.py
@@ -0,0 +1,43 @@
+import json
+import unittest
+
+from api import create_app, db
+from tests import db_drop_everything, assert_payload_field_type_value, \
+    assert_payload_field_type
+
+
+class ForecastTest(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.session.remove()
+        db_drop_everything(db)
+        self.app_context.pop()
+
+    def test_forecast_happypath(self):
+        response = self.client.get('/api/forecast?location=Arvada,CO')
+        data = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(200, response.status_code)
+        assert_payload_field_type_value(self, data, 'success', bool, True)
+
+        assert_payload_field_type_value(
+            self, data, 'location', str, 'Arvada, CO'
+        )
+
+        assert_payload_field_type(self, data, 'current_temp', str)
+        self.assertEqual('F', data['current_temp'][-1])
+
+        assert_payload_field_type(self, data, 'conditions', str)
+        self.assertGreater(0, len(data['conditions']))
+
+# {
+#   "location": "Arvada, CO",
+#   "current_temp": "79.3F",
+#   "conditions": "partly cloudy"
+# }
